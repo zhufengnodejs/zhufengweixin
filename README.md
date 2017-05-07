dddf5f7a553cc21aeb6e1f459be02f0a5c51ab26
zhufengnodejs <zhufengnodejs@126.com>
2017-05-07 20:07:08 +0800

    增加demo接口

---
+++ README.md
@@ -0,0 +1,10 @@
+## 1. 下载项目
+```
+git clone https://github.com/zhufengnodejs/zhufengweixin.git
+```
+
+## 2.安装依赖的模块
+```
+cd zhufengweixin
+npm install
+```
--- routes/pages.js
+++ routes/pages.js
@@ -18,4 +18,20 @@ router.get('/record', async function (req, res) {
         signature
     });
 });
+
+
+router.get('/demo', async function (req, res) {
+    let ticket = await getTicket();
+    let nonceStr = Math.random().toString(36).substr(2,15);
+    let timestamp = parseInt(Date.now()/1000);
+    //注意参数名全是小写
+    let str = [`noncestr=${nonceStr}`,`jsapi_ticket=${ticket}`,`timestamp=${timestamp}`,`url=http://6c24537b.ngrok.io/pages/record`].sort().join('&');
+    let signature  =  crypto.createHash('sha1').update(str).digest('hex');
+    res.render('record',{
+        appId:appID,
+        timestamp,
+        nonceStr,
+        signature
+    });
+});
 module.exports = router;
